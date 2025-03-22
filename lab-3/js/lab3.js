aabc77451b967332c049bad51b37f51d0fabd1cd
Susan S. <susan.sotosarmiento@mygeorgian.ca>
2025-03-21 23:53:12 -0400

    second try of js

--- lab-3/js/lab3.js
+++ lab-3/js/lab3.js
@@ -1,19 +1,32 @@
-const tabs = document.querySelectorrAll(`[role="tab"]`);
+// const tabs = document.querySelectorrAll(`[role="tab"]`);
 
-tabs.forEach((tab) => {
-	tab.addEventListener("click", (event) => {
-		event.preventDefault();
+// tabs.forEach((tab) => {
+// 	tab.addEventListener("click", (event) => {
+// 		event.preventDefault();
 
-		panels.forEach((panel) => panel.style.display = "none");
+// 		panels.forEach((panel) => panel.style.display = "none");
 
-		tabs.forEach((tab) => tab.classList.remove("active"));
+// 		tabs.forEach((tab) => tab.classList.remove("active"));
 
-		const targetPanelId = tab.querySelector("a").getAttribute("href");
-		document.querySelector(targetPanelId).style.display = "block";
+// 		const targetPanelId = tab.querySelector("a").getAttribute("href");
+// 		document.querySelector(targetPanelId).style.display = "block";
 
-		tab.classList.add("active");
-	});
-});
+// 		tab.classList.add("active");
+// 	});
+// });
 
-tabs[0].classList.add("active");
-document.querySelector(`[role="tabpanel"]`).style.display = "block";
+// tabs[0].classList.add("active");
+// document.querySelector(`[role="tabpanel"]`).style.display = "block";
+
+
+
+const tabs = document.querySelectorAll(`[role="tab"]`);
+
+
+
+
+
+
+var tabpanel1 = document.querySelector("#tabpanel1");
+var tabpanel2 = document.querySelector("#tabpanel2");
+var tabpanel3 = document.querySelector("#tabpanel3");
